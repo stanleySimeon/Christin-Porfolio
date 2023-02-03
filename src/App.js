cd90c8b0f0be30dc3809641c4eaed69a18b1e3fd
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-03 16:00:03 -0500

    Project setup and navigation bar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigation from './components/navigation/Navigation';
+
+function App() {
+  return (
+    <div className="container-fluid p-0 overflow-x-hidden">
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        christin: {
+          DEFAULT: '#000518',
+          primary: '#00D2EE',
+          secondary: '#D6543F',
+          hover: '#0595A8',
+          gray: '#56585E',
+          grayLight: '#F4F4F4',
+          white: '#ffffff',
+        },
+      },
+      container: {
+        center: true,
+        width: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+        },
+      },
+    },
+  },
+  /* eslint-disable global-require */
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
+};
